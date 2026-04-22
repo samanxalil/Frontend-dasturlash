5db2d26694031543b1dd1ae0a9db8f972c77ebf7
samandar <161134751+samanxalil@users.noreply.github.com>
2026-04-22 09:39:27 +0500

    update4

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-/*
-1 misol
+
 let son = prompt ("son kiriting:");
 if (son % 2 === 0){
  console.log("Juft son");
@@ -7,7 +6,7 @@ if (son % 2 === 0){
     console.log("Toq son");
     
 }
-2misol
+
  let a = +prompt("Birinchi sonni kiriting:");
 let b = +prompt("Ikkinchi sonni kiriting:");
 
@@ -18,16 +17,16 @@ if (a > b) {
 } else {
     console.log("Ular teng");
 } 
-    3misol
+
     let tugilganYil = prompt("Tug'ilgan yilingizni kiriting:");
 let joriyYil = new Date().getFullYear();
 let yosh = joriyYil - tugilganYil;
 console.log("Sizning yoshingiz: " + yosh);
-4 misol
+
 let x = 10, y = 25, z = 15;
 let max = Math.max(x, y, z);
 console.log("Eng kattasi: " + max);
-5 misol
+
 let n1 = +prompt("1-son:");
 let n2 = +prompt("2-son:"); 
 let amal = prompt("Amalni kiriting (+, -, *, /):");
@@ -39,15 +38,15 @@ switch(amal) {
     case '/': console.log(n1 / n2); break;
     default: console.log("Noma'lum amal");
 }
-    6misol
+ 
     let n = +prompt("n ni kiriting:");
 let yigindi = 0;
 for (let i = 1; i <= n; i++) {
     yigindi += i;
 }
 console.log("Yig'indi: " + yigindi);
-7 misol
+
 let son = +prompt("Qaysi sonning ko'paytirish jadvali kerak?");
 for (let i = 1; i <= 10; i++) {
     console.log(`${son} x ${i} = ${son * i}`);
-}*/
+}
