55a8008ce6ef7e08f0011e56c8e9ade511367ad0
samandar <161134751+samanxalil@users.noreply.github.com>
2026-04-28 10:54:37 +0500

    oppiip

--- main.js
+++ main.js
@@ -1,56 +1,81 @@
 let son = 8; // Masalan
 if (son % 2 === 0) {
-    console.log("Juft son");
-} else
-     {
-   
-        console.log("Toq son");
-}
-let a = 15, b = 25;
-if (a > b) {
-    console.log(a);
+  console.log("Juft son");
 } else {
-    console.log(b);
+  console.log("Toq son");
+}
+let a = 15,
+  b = 25;
+if (a > b) {
+  console.log(a);
+} else {
+  console.log(b);
 }
 let ball = 85;
 if (ball > 90) {
-    console.log("A'lo");
+  console.log("A'lo");
 } else if (ball > 70) {
-    console.log("Yaxshi");
+  console.log("Yaxshi");
 } else if (ball > 50) {
-    alert.log("Qoniqarli");
+  console.log("Qoniqarli");
 } else {
-    alert("Yiqildi");
+  console.log("Yiqildi");
 }
 for (let i = 1; i <= 15; i++) {
-    alert(i * i);
+  console.log(i * i);
 }
 for (let i = 10; i >= 1; i--) {
-   alert(i);
-}for (let i = 1; i <= 100; i++) {
-    if (i % 5 === 0) {
-        alert(i);
-    }
+  console.log(i);
 }
-let n = 20; 
+for (let i = 1; i <= 100; i++) {
+  if (i % 5 === 0) {
+    console.log(i);
+  }
+}
+let n = 20;
 for (let i = 1; i <= n; i++) {
-    if (i % 2 !== 0) {
-        console.log(i);
-    }
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
 }
 let oy = 4;
 switch (oy) {
-    case 1: console.log("Yanvar"); break;
-    case 2: console.log("Fevral"); break;
-    case 3: console.log("Mart"); break;
-    case 4: console.log("Aprel"); break;
-    case 5: console.log("May"); break;
-    case 6: console.log("Iyun"); break;
-    case 7: console.log("Iyul"); break;
-    case 8: console.log("Avgust"); break;
-    case 9: console.log("Sentabr"); break;
-    case 10: console.log("Oktabr"); break;
-    case 11: console.log("Noyabr"); break;
-    case 12: console.log("Dekabr"); break;
-    default: console.log("Bunday oy yo'q");
+  case 1:
+    console.log("Yanvar");
+    break;
+  case 2:
+    console.log("Fevral");
+    break;
+  case 3:
+    console.log("Mart");
+    break;
+  case 4:
+    console.log("Aprel");
+    break;
+  case 5:
+    console.log("May");
+    break;
+  case 6:
+    console.log("Iyun");
+    break;
+  case 7:
+    console.log("Iyul");
+    break;
+  case 8:
+    console.log("Avgust");
+    break;
+  case 9:
+    console.log("Sentabr");
+    break;
+  case 10:
+    console.log("Oktabr");
+    break;
+  case 11:
+    console.log("Noyabr");
+    break;
+  case 12:
+    console.log("Dekabr");
+    break;
+  default:
+    console.log("Bunday oy yo'q");
 }
